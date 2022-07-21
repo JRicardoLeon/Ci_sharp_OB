f6a57391e1de125d8262c443a11bcd4d02bd2122
JRicardoLeon <josericardobl1@outlook.com>
2022-07-21 18:12:48 -0500

    Solucion del primer reto del bootcamp Sesión 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+// EJERCICIO 1
+Console.WriteLine("Escriba su nombre...!");
+String nombre = Console.ReadLine();
+
+Console.WriteLine("Hola mi nombre es: "+nombre);
+
+
+// EJERCICIO 2
+Console.WriteLine("Escriba su nombre...!");
+String hora = DateTime.Now.ToString("hh:mm:ss");
+
+Console.WriteLine("La hora del sistema actual es: " + hora);
