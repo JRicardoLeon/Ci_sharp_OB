9321950e1d3279c87d5abfcd4e7dacd28d045bea
JRicardoLeon <josericardobl1@outlook.com>
2022-07-21 23:05:39 -0500

    Solucion del reto 2

---
+++ ClassMain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo1
+{
+    class OB_Ci_Sharp
+    {
+        static void Main(string[] args)
+        {
+
+            Reto2 x = new Reto2();
+            Console.WriteLine(x.ejercicio1());
+        }
+        static void ejercicio2()
+        {
+           Console.WriteLine("Escriba su nombre...!");
+            String nombre = Console.ReadLine();
+
+            Console.WriteLine("Hola mi nombre es: " + nombre);
+
+            // EJERCICIO 2
+            String hora = DateTime.Now.ToString("hh:mm:ss");
+
+            Console.WriteLine("La hora del sistema actual es: " + hora);
+           
+            Console.WriteLine("soy un metodo");
+        }
+        
+    }
+}
---
+++ Sesion2.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo1
+{
+    internal class Reto2
+    {
+        public String ejercicio1()
+        {
+
+            Console.WriteLine("Escriba su nombre...!");
+            String nombre = Console.ReadLine();
+
+            Console.WriteLine("Escriba su apellido...!");
+            String apellido = Console.ReadLine();
+
+            Console.WriteLine("Escriba su edad...!");
+            int edad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sabes Programar...?");
+            String dev = Console.ReadLine();
+            String mensaje = ("Hola mi nombre es: " + nombre +" "+ apellido+
+                " tengo "+edad+" años" +dev+" "+"se programar");
+
+
+            return mensaje;
+        }
+        public String ejercicio2()
+        {
+            Console.WriteLine("Escriba Cuantas puertas tiene su Carro...!");
+            int puertas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escriba Cuantas llantas tiene su Carro...!");
+            int llantas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escriba la marca del Carro...!");
+            String marca = Console.ReadLine();
+
+            Console.WriteLine("Escriba el codigo ITV vigente de su Carro...!");
+            int ivt = int.Parse(Console.ReadLine());
+
+
+            String carro = ("Mi Carro tiene" + puertas + "y " + llantas +
+                " es de marca " + marca + " con ITV vigente" + ivt);
+            return carro;
+        }
+
+        public void ejercicio3()
+        {
+            Console.WriteLine("Digite un Numero...!");
+            int numero = int.Parse(Console.ReadLine());
+            if (numero >= 18)
+            {
+                Console.WriteLine("El numero que diito es Mayor que 18...!");
+            }
+
+            if (numero >= 0)
+            {
+                if (numero >= 18)
+                {
+                    Console.WriteLine("El numero es positivo y mayor que 18.");
+                }
+                else
+                {
+                    Console.WriteLine("El numero es Menor que 18.");
+                }
+               
+            }
+            Char caracter = 'a';
+            Console.WriteLine(caracter);
+        }
+    }
+}
